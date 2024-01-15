f45041142b68309eccecae0f118fcb401fd1a6ca
1ronlion <leonardo.bana89@gmail.com>
2024-01-15 19:51:39 -0300

    resuelto clase 42

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { calculate, Operation } from "./calculator";
 import { argsParser } from "./args-parser";
+import { sumarMayores } from "./promedios";
 
 function main (){
 
-    let params = argsParser(process.argv)
-    console.log("🚀 ~ main ~ params:", params)
-    let operation = process.argv[4] as Operation
-    let result = calculate(params[0], params[1], operation)
-    console.log(result);
+    // let params = argsParser(process.argv)
+    // console.log("🚀 ~ main ~ params:", params)
+    // let operation = process.argv[4] as Operation
+    // let result = calculate(params[0], params[1], operation)
+    // console.log(result);
+
+    let numbers = [2, 10, 16, 20, 22]
+    let result = sumarMayores(numbers)
+    console.log("🚀 ~ main ~ result:", result)
 
 }
 
--- promedios.ts
+++ promedios.ts
@@ -1,13 +1,34 @@
 // Dado un array de numeros, imprimir la suma de los numeros en el array
 // que sean mayores al promedio del array 
 
-function calcularPromedio (){
 
+function sumaDeNumeros (arr: Array <number>): number{
+
+    const resultado = arr.reduce((acumulador, indice) => acumulador + indice, 0)
+
+    return resultado
 
 }
 
-function sumarMayores (){
 
+export function calcularPromedio (arr: Array <number>){
 
+   let suma = sumaDeNumeros(arr)
+   let promedio = suma / arr.length
+   let resultado = Math.trunc(promedio)
+   //Math.round toFixed()
+
+   return resultado
+}
+
+export function sumarMayores (arr: Array <number>){
+
+    let promedio = calcularPromedio(arr)
+
+    let arrayFiltrado = arr.filter((element) => element > promedio)
+
+    let resultado = sumaDeNumeros(arrayFiltrado)
+
+    return resultado
     
 }
