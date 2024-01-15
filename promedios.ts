576cc1da79610eba60c5a3f661006de7994e8c66
1ronlion <leonardo.bana89@gmail.com>
2024-01-15 16:14:52 -0300

    actualizando consignas

---
+++ promedios.ts
@@ -0,0 +1,13 @@
+// Dado un array de numeros, imprimir la suma de los numeros en el array
+// que sean mayores al promedio del array 
+
+function calcularPromedio (){
+
+
+}
+
+function sumarMayores (){
+
+
+    
+}
